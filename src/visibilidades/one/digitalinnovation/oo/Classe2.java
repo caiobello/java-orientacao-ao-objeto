70bf50cc68709c327c35757f83660a4009fade53
Caio Bello Zamana <caiobzm@gmail.com>
2023-05-10 16:22:55 -0300

    Att

---
+++ src/visibilidades/one/digitalinnovation/oo/Classe2.java
@@ -0,0 +1,17 @@
+package visibilidades.one.digitalinnovation.oo;
+/**
+ * Classe de exemplo para o exercício da Aula 5 de Orientação a Objetos.
+ */
+class Classe2 extends Classe1 {
+
+    //Atributos próprios
+
+    void metodo() {
+
+        //atributo2 e 3
+        
+    }
+
+    //metodo2 e 3
+
+}
